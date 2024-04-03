bd8b74ee2626132e0f43697308d0405822726b3d
Evan McKinney <evmckinney9@gmail.com>
2024-04-03 13:34:13 -0400

    feat: proton class

--- src/template_demo/main.py
+++ src/template_demo/main.py
@@ -33,3 +33,37 @@ class Electron:
         :raises ValueError: You did something wrong
         """
         return self.mass * velocity
+
+
+class Proton:
+    """A representation of a proton."""
+
+    def __init__(self, size: str = None, name: str = None) -> None:
+        """Create a proton.
+
+        :param size: How big should this thing be?
+        :param name: The name we'll call the proton. Nicknames
+            preferred.
+        :raises ValueError: You did something wrong
+        """
+        self.size = size
+        self.name = name
+
+    @property
+    def charge(self):
+        """The charge of the proton."""
+        return 1
+
+    @property
+    def mass(self) -> float:
+        """The mass of the proton."""
+        return 1837 * Electron().mass
+
+    def compute_momentum(self, velocity: float) -> float:
+        """Compute the proton's velocity.
+
+        :param velocity: The proton's velocity
+        :return: The computed momentum.
+        :raises ValueError: You did something wrong
+        """
+        return self.mass * velocity
